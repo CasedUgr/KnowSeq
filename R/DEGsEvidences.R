df78126882fc77dd36a17fc2ae6d3c4cb1a88463
marveral <marveral333@gmail.com>
2020-02-11 09:56:59 +0100

    Add new parameter to search evidences for a subtype of a disease.

--- R/DEGsEvidences.R
+++ R/DEGsEvidences.R
@@ -1,11 +1,14 @@
 
 #' @param geneList A list that contains the gene symbols or gene names of the DEGs.
 #' @param disease The name of a disease in order to calculate the Disease Association ranking by using the DEGs indicated in the vars_selected parameter.
+#' @param subdisease The name of a particular subdisease from disease in order to calculate the Disease Association ranking by using the DEGs indicated in the vars_selected parameter. Default '' 
 #' @param minCitation Minimum number of citations of each genes in a disease to consider the genes related with the disease.
 #' @param size The number of diseases to retrieve from targetValidation
 #' @return A list which names are genes from geneList and which contains related evidences for each gene in geneList and indicated disease.
+#' @example
+#' evidences <- DEGsToDiseases(c("KRT19","BRCA1"),'cancer')
 
-DEGsEvidences <- function(geneList, disease, minCitation = 5, size = 10){
+DEGsEvidences <- function(geneList, disease, subdisease='', minCitation = 5, size = 10){
   if(length(geneList)[1] == 0 || is.null(geneList) ){
     
     stop("The geneList is empty! Please, provide a right geneList.")
@@ -17,99 +20,119 @@ DEGsEvidences <- function(geneList, disease, minCitation = 5, size = 10){
     
   }
   
-  # Get disease id
+  # Get disease id (it's necesary for evidences request)
   r_Ensembl <- httr::GET(paste("https://api.opentargets.io/v3/platform/public/search?q=",disease,"&size=1&filter=disease",sep = ""))
   respon <- content(r_Ensembl)
   if (respon$size == 0){
     stop("Disease not found")
   }
   disease.id <- respon$data[[1]]$id
-
+  
   cat("Obtaining related diseases with the DEGs from targetValidation platform...\n")
   base = "https://api.opentargets.io/v3/platform/public/evidence/filter?target="
   
-  genePeti = character()
-  results <- vector("list", 0)
+  # Create empty output
   info <- list()
   
-  for(j in seq_len(length(unique(geneList)))){
-    # Get gene id
+  # Iter in genes from geneList
+  for(j in seq(length(unique(geneList)))){
+    # Get gene id (it's necesary for evidences request)
     r_Ensembl <- httr::GET(paste("https://api.opentargets.io/v3/platform/public/search?q=",geneList[j],"&size=1&filter=target",sep = ""))
     respon <- content(r_Ensembl)
     if( length(respon$data) > 0 )ensembl_id <- respon$data[[1]]$data$ensembl_gene_id
     else ensembl_id <- NULL
     
-    
+    # If gene is found
     if(length(ensembl_id) > 0){
-      disease.req <- paste(base,ensembl_id,"&disease=",disease.id,"&direct=true&size=100",sep = "")
+      disease.req <- paste(base,ensembl_id,"&disease=",disease.id,"&direct=true&size=",size,sep = "")
       disease.req <- GET(disease.req)
       response.disease = content(disease.req)
       
+      # If response is not empty
       if(response.disease$size != 0){
-        act.evidences = matrix(, nrow = response.disease$size, ncol = 6)
+        # Empty matrix that will contain information from obtained evidences for actual gene
+        evidences = matrix(,ncol = 6)
         
+        
+        # Iter in found evidences
         for(k in seq(response.disease$size)){
-          if (response.disease$data[[k]]$target$gene_info$geneid == ensembl_id){
-            type <- response.disease$data[[k]]$type
-            act.evidences[k,1] = type
-            act.evidences[k,2] = response.disease$data[[k]]$evidence$evidence_codes[[1]]
-            act.evidences[k,3] = response.disease$data[[k]]$scores$association_score
-
-            if (type == 'known_drug'){
-              act.evidences[k,4] = response.disease$data[[k]]$drug$molecule_name
-              act.evidences[k,5] = response.disease$data[[k]]$drug$molecule_type
-            }
-            else if (type == 'literature'){
-              act.evidences[k,4] = response.disease$data[[k]]$evidence$literature_ref$lit_id # url
-            }
-            else if(type == 'affected_pathway'){
-              if (length(response.disease$data[[k]]$evidence$resource_score$method$reference) > 0){
-                act.evidences[k,4] = response.disease$data[[k]]$evidence$resource_score$method$reference # url
+          # Check if disease is matching
+          if ( (subdisease=='' && grepl(disease,response.disease$data[[k]]$disease$efo_info$label))
+               || subdisease == response.disease$data[[k]]$disease$efo_info$label){
+            
+            # Actual evidence (empty)
+            act.evidences <- rep('*',6)
+            
+            # Check if gene is matching
+            if (response.disease$data[[k]]$target$gene_info$geneid == ensembl_id){
+              # Save evidence type, code and score
+              type <- response.disease$data[[k]]$type
+              act.evidences[1] = type
+              act.evidences[2] = response.disease$data[[k]]$evidence$evidence_codes[[1]]
+              act.evidences[3] = response.disease$data[[k]]$scores$association_score
+              
+              # Save information depending on evidence type
+              if (type == 'known_drug'){
+                act.evidences[4] = response.disease$data[[k]]$drug$molecule_name # Drug name
+                act.evidences[5] = response.disease$data[[k]]$drug$molecule_type # Molecule type
               }
-              if (length(response.disease$data[[k]]$evidence$known_mutations)>0){
-                act.evidences[k,5] = response.disease$data[[k]]$evidence$known_mutations[[1]]$preferred_name
+              else if (type == 'literature'){
+                act.evidences[4] = response.disease$data[[k]]$evidence$literature_ref$lit_id # paper url
               }
-              if(length( response.disease$data[[k]]$evidence$urls[[1]]$url ) > 0){
-                act.evidences[k,6] = response.disease$data[[k]]$evidence$urls[[1]]$url # reactome url
+              else if(type == 'affected_pathway'){
+                if (length(response.disease$data[[k]]$evidence$resource_score$method$reference) > 0){
+                  act.evidences[4] = response.disease$data[[k]]$evidence$resource_score$method$reference # paper url
+                }
+                if (length(response.disease$data[[k]]$evidence$known_mutations)>0){
+                  act.evidences[5] = response.disease$data[[k]]$evidence$known_mutations[[1]]$preferred_name
+                }
+                if(length( response.disease$data[[k]]$evidence$urls[[1]]$url ) > 0){
+                  act.evidences[6] = response.disease$data[[k]]$evidence$urls[[1]]$url # reactome url
+                }
+              }
+              else if(type == 'animal_model'){
+                act.evidences[4] = response.disease$data[[k]]$evidence$biological_model$is_associated # Boolean
+                act.evidences[5] = response.disease$data[[k]]$evidence$biological_model$species       # Specie
+              }
+              else if (type == 'rna_expression'){
+                if(length(response.disease$data[[k]]$literature$references[[1]]$lit_id>0))
+                  act.evidences[4] = response.disease$data[[k]]$literature$references[[1]]$lit_id[1] # paper url
+                act.evidences[5] = response.disease$data[[k]]$evidence$comparison_name # ej: "'oral squamous cell carcinoma' vs 'normal'"
+              }
+              else if (type == 'genetic_association'){
+                if(length(response.disease$data[[k]]$literature$references[[1]]$list_id > 0))
+                  act.evidences[4] = response.disease$data[[k]]$literature$references[[1]]$lit_id # paper url
+                if(length(response.disease$data[[k]]$evidence$gene2variant$functional_consequence)>0)
+                  act.evidences[5] = response.disease$data[[k]]$evidence$gene2variant$functional_consequence # url ej: ontobee
+              }
+              else if (type == 'somatic_mutation'){
+                act.evidences[4] = response.disease$data[[k]]$evidence$known_mutations[[1]]$functional_consequence # url ej: ontobee
+                act.evidences[5] = response.disease$data[[k]]$evidence$known_mutations[[1]]$preferred_name # ej: "sequence_alteration"
+                if (length(response.disease$data[[k]]$evidence$known_mutations[[1]]$role_in_cancer) > 0)
+                  act.evidences[6] = response.disease$data[[k]]$evidence$known_mutations[[1]]$role_in_cancer # ej: "oncogene, TSG"
+              }
+              else{
+                print(paste('Unknown evidence type',response.disease$data[[k]]$type))
               }
             }
-            else if(type == 'animal_model'){
-              act.evidences[k,4] = response.disease$data[[k]]$evidence$biological_model$is_associated
-              act.evidences[k,5] = response.disease$data[[k]]$evidence$biological_model$species
-            }
-            else if (type == 'rna_expression'){
-              if(length(response.disease$data[[k]]$literature$references[[1]]$lit_id>0))
-                act.evidences[k,4] = response.disease$data[[k]]$literature$references[[1]]$lit_id[1]
-              act.evidences[k,5] = response.disease$data[[k]]$evidence$comparison_name # ej: "'oral squamous cell carcinoma' vs 'normal'"
-            }
-            else if (type == 'genetic_association'){
-              if(length(response.disease$data[[k]]$literature$references[[1]]$list_id > 0))
-                act.evidences[k,4] = response.disease$data[[k]]$literature$references[[1]]$lit_id # paper
-              if(length(response.disease$data[[k]]$evidence$gene2variant$functional_consequence)>0)
-                act.evidences[k,5] = response.disease$data[[k]]$evidence$gene2variant$functional_consequence # url
-            }
-            else if (type == 'somatic_mutation'){
-              act.evidences[k,4] = response.disease$data[[k]]$evidence$known_mutations[[1]]$functional_consequence #url
-              act.evidences[k,5] = response.disease$data[[k]]$evidence$known_mutations[[1]]$preferred_name # ej: "sequence_alteration"
-              if (length(response.disease$data[[k]]$evidence$known_mutations[[1]]$role_in_cancer) > 0)
-                act.evidences[k,6] = response.disease$data[[k]]$evidence$known_mutations[[1]]$role_in_cancer # ej: "oncogene, TSG"
-            }
-            else{
-              print(paste('Unknown evidence type',response.disease$data[[k]]$type))
-            }
+            # Save actual evidences 
+            evidences <- rbind(evidences,act.evidences)
           }
         }
-        info[[geneList[j]]] <- act.evidences
+        # Save found evidences for gene j (first row is empty)
+        info[[geneList[j]]] <- evidences[-1,]
       }else{
+        # Not evidences found for gene j.
+        # Add empty evidence
         info[[geneList[j]]] <- rbind(c('literature',rep('*',5)),c('literature',rep('*',5)))
-        cat(paste( unique(geneList)[j], " isn't associated with ",disease,".\n",sep = ""))
       }
     }else{
+      # Gene not found. 
+      # Add empty evidence
       info[[geneList[j]]] <- rbind(c('literature',rep('*',5)),c('literature',rep('*',5)))
     }
   }
   
   cat("Evidences acquired successfully!\n")
-  results <- results[lengths(results) != 0]
   invisible(info)
 }

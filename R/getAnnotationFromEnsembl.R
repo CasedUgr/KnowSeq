b703ea7eff9b9ce5744858e1a80ae9c7bcecdc88
marveral <marveral333@gmail.com>
2020-02-13 15:08:04 +0100

    Replace biomart by web queries, downloading full genome, when database is not human.

--- R/getAnnotationFromEnsembl.R
+++ R/getAnnotationFromEnsembl.R
@@ -9,8 +9,11 @@
 #' @return A matrix that contains all the information asked to the attributes parameter.
 #' @examples
 #' myAnnotation <- getAnnotationFromEnsembl(c("ENSG00000210049","ENSG00000211459","ENSG00000210077"),notHSapiens=FALSE)
+#' myAnnotation <- getAnnotationFromEnsembl(c("MGP_129S1SvImJ_G0038602", "MGP_129S1SvImJ_G0007718","MGP_129S1SvImJ_G0023218"),notHSapiens = TRUE, notHumandataset = 'mm129s1svimj_gene_ensembl')
 
-getAnnotationFromEnsembl <- function(values,attributes=c("ensembl_gene_id","external_gene_name","percentage_gene_gc_content","gene_biotype"), filters="ensembl_gene_id", notHSapiens = FALSE, notHumandataset = ""){
+
+
+getAnnotationFromEnsembl <- function(values,attributes=c("ensembl_gene_id","external_gene_name","percentage_gene_gc_content"), filters="ensembl_gene_id", notHSapiens = FALSE, notHumandataset = ""){
 
   if(typeof(attributes) != "character"){stop("The parameter attributes must be a character vector that contains the wanted ensembl attributes.")}
   if(typeof(values) != "character"){stop("The parameter values must be a character vector that contains the genes IDs.")}
@@ -27,13 +30,35 @@ getAnnotationFromEnsembl <- function(values,attributes=c("ensembl_gene_id","exte
   }else{
 
     if(length(notHumandataset)[1] == 0 || is.null(notHumandataset)){stop("The notHumandataset is empty! Please, provide a right notHumandataset")}
+    
+    filename <- paste('inst/extdata/',notHumandataset,'.csv',sep='')
 
-    cat(paste("Downloading annotation ", notHumandataset, "...\n"))
-    mart <- useMart("ensembl", dataset=notHumandataset)
+    if (file.exists(filename)){
+      cat(paste("Getting annotation ", notHumandataset, "...\n"))
+      myAnnotation <- read.csv(filename,header=TRUE)
+    }
+    else{
+      cat(paste("Downloading annotation ", notHumandataset, "...\n"))
+      query = paste('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE Query>
+          <Query  virtualSchemaName = "default" formatter = "CSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >
+          <Dataset name = "',notHumandataset,'" interface = "default" >',sep='')
+      for (attribute in attributes)
+        query <- paste(query,'<Attribute name = "',attribute,'" />',sep='')
+      query <- paste(query,'</Dataset></Query>',sep='')
+    
+      request <- paste('http://www.ensembl.org/biomart/martservice?query=',query,sep='')
+      download.file(request,method='wget',destfile = filename)
+      myAnnotation <- read.csv(filename)
+      
+      # Set columns names as attributes
+      colnames(myAnnotation) <- attributes
+      write.csv(myAnnotation,filename,row.names=FALSE)
 
-    myAnnotation <- getBM(attributes = attributes, filters = filters, values = values, mart = mart)
-
+    }
+    myAnnotation <- myAnnotation[myAnnotation$ensembl_gene_id %in% values,]
   }
   return(myAnnotation)
-
 }
+
+
+

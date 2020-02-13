1731e55713a7742203b7b5cc3f822ee735aafe50
marveral <marveral333@gmail.com>
2020-02-13 13:16:01 +0100

    Return annotation just of selected genes

--- R/getAnnotationFromEnsembl.R
+++ R/getAnnotationFromEnsembl.R
@@ -8,7 +8,7 @@
 #' @param notHumandataset A dataset identification from biomaRt::listDatasets(useMart("ensembl")).
 #' @return A matrix that contains all the information asked to the attributes parameter.
 #' @examples
-#' myAnnotation <- getAnnotationFromEnsembl(c("ENSG00000210049","ENSG00000211459","ENSG00000210077"),referenceGenome=37)
+#' myAnnotation <- getAnnotationFromEnsembl(c("ENSG00000210049","ENSG00000211459","ENSG00000210077"),notHSapiens=FALSE)
 
 getAnnotationFromEnsembl <- function(values,attributes=c("ensembl_gene_id","external_gene_name","percentage_gene_gc_content","gene_biotype"), filters="ensembl_gene_id", notHSapiens = FALSE, notHumandataset = ""){
 
@@ -23,7 +23,7 @@ getAnnotationFromEnsembl <- function(values,attributes=c("ensembl_gene_id","exte
       cat(paste("Using reference genome 38.\n"))
 
       myAnnotation <- read.csv('inst/extdata/GRCh38Annotation.csv')
-
+      myAnnotation <- myAnnotation[myAnnotation$ensembl_gene_id %in% values,]
   }else{
 
     if(length(notHumandataset)[1] == 0 || is.null(notHumandataset)){stop("The notHumandataset is empty! Please, provide a right notHumandataset")}

d1f01c690fa35fcf43094104c91b23da38eba1b4
marveral <marveral333@gmail.com>
2020-04-04 20:25:32 +0100

    Save plots in png or pdf

--- R/RNAseqQA.R
+++ R/RNAseqQA.R
@@ -1,23 +1,29 @@
 #' RNAseqQA performs the quality analysis of an expression matrix.
 #'
-#' RNAseqQA performs the quality analysis of an expression matrix. This function adapts the RNA-seq data in order to allows using arrayQualityMetrics expression analysis.
+#' RNAseqQA performs the quality analysis of an expression matrix. This function generates different plots over expression data in order to detect possible outliers.
 #' @param expressionMatrix A matrix that contains the gene expression values.
 #' @param outdir The output directory to store the report of arrayQualityMetrics
+#' @param toPNG Boolean variable to indicate if a plot would be save to PNG.
+#' @param toPDF Boolean variable to indicate if a plot would be save to PDF.
 #' @return Nothing to return.
 #' @examples
 #' dir <- system.file("extdata", package="KnowSeq")
 #' load(paste(dir,"/expressionExample.RData",sep = ""))
-#'
-RNAseqQA(expressionMatrix)
+#' RNAseqQA(expressionMatrix)
 
-RNAseqQA <- function(expressionMatrix, outdir = "RNAseqQA"){
+
+RNAseqQA <- function(expressionMatrix, outdir = "myPlots", toPNG = TRUE, toPDF = TRUE){
   
   if(!is.matrix(expressionMatrix)){stop("The class of expressionMatrix parameter must be matrix.")}
+  if(!is.logical(toPNG)){stop("toPNG parameter can only take the values TRUE or FALSE.")}
+  if(!is.logical(toPDF)){stop("toPDF parameter can only take the values TRUE or FALSE.")}
+  
+  if(! dir.exists(outdir)) system(paste("mkdir",outdir))
   
   cat("Performing samples quality analysis...\n")
   
   expressionMatrix <- expressionMatrix[unique(rownames(expressionMatrix)),]
-  num.samples <- num.samples
+  num.samples <- ncol(expressionMatrix)
   
   # Array distances matrix
   melted_cormat <- melt(cor(expressionMatrix))
@@ -25,8 +31,8 @@ RNAseqQA <- function(expressionMatrix, outdir = "RNAseqQA"){
     geom_tile() + ggtitle('Distances between arrays') + 
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) + xlab('') + ylab('')
   #distance.plot
-  ggsave(paste(outdir,'distance-plot.png',sep='/'),distance.plot)
-
+  if (toPNG) ggsave(paste(outdir,'distance-plot.png',sep='/'),distance.plot)
+  if (toPDF)  ggsave(paste(outdir,'distance-plot.pdf',sep='/'),distance.plot)
 
   # BOXPLOT
   boxplot.data <- melt(expressionMatrix)
@@ -35,8 +41,8 @@ RNAseqQA <- function(expressionMatrix, outdir = "RNAseqQA"){
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     geom_boxplot(outlier.colour="red", outlier.shape=8, outlier.size=4,fill='#56B4E9') 
   #box.plot
-  ggsave(paste(outdir,'box-plot.png',sep='/'),box.plot)
-  
+  if (toPNG) ggsave(paste(outdir,'box-plot.png',sep='/'),box.plot)
+  if (toPDF) ggsave(paste(outdir,'box-plot.pdf',sep='/'),box.plot)
   
   # KS
   # Empirical cumulative distribution function
@@ -50,7 +56,8 @@ RNAseqQA <- function(expressionMatrix, outdir = "RNAseqQA"){
     scale_y_discrete(limits=c(1:num.samples),labels=colnames(expressionMatrix)) + ylab('') +
     theme_classic()
   #ks.plot
-  ggsave(paste(outdir,'ks-plot.png',sep='/'),ks.plot)
+  if (toPNG) ggsave(paste(outdir,'ks-plot.png',sep='/'),ks.plot)
+  if (toPDF) ggsave(paste(outdir,'ks-plot.pdf',sep='/'),ks.plot)
   
   # MA-PLOT
   plots <- list()
@@ -65,5 +72,6 @@ RNAseqQA <- function(expressionMatrix, outdir = "RNAseqQA"){
   }
   ma.plots <- do.call(grid.arrange,c(plots,ncol=2))
   #ma.plots
-  ggsave(paste(outdir,'MA-plot.png',sep='/'),ma.plots)
+  if (toPNG) ggsave(paste(outdir,'MA-plot.png',sep='/'),ma.plots)
+  if (toPDF) ggsave(paste(outdir,'MA-plot.pdf',sep='/'),ma.plots)
 }
